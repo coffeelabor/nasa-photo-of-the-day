d34c02d69ad9a6a5b05dffb5650b44986f52328c
Reed James <reedtjames1234@gmail.com>
2019-07-10 17:02:59 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [nasaPhoto, setNasaPhoto] = useState([]);
+  console.log("nasaPhoto Log", nasaPhoto);
+
+  const fetchNasaPhoto = () => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=Di0ZiC8Wm0h8bPQPo6iCu8byCqbJkgLvFpUL2swW&date=2012-03-14`
+    )
+      .then(res => res.json())
+      // .then(res => console.log("RES LOG 1", res))
+      .then(res => {
+        setNasaPhoto(res);
+      })
+      // .then(res => setNasaPhoto(res.explanation))
+      // .then(console.log("nasaPhoto after set", nasaPhoto))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchNasaPhoto();
+  }, []);
+
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        app! Have fun !
+      </p> */}
+      <h1>{nasaPhoto.explanation}</h1>
+      <img src={nasaPhoto.url} />
     </div>
   );
 }

3bd0eec86488a47a50b96e58534cf62c2b98825b
Reed James <reedtjames1234@gmail.com>
2019-07-11 15:57:51 -0500

    Styling done, working on testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Grid, Container, Image, Header, Icon } from "semantic-ui-react";
 import "./App.css";
-
+import "semantic-ui-css/semantic.min.css";
 function App() {
   const [nasaPhoto, setNasaPhoto] = useState([]);
   console.log("nasaPhoto Log", nasaPhoto);
@@ -24,14 +25,32 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {/* <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun !
-      </p> */}
-      <h3>{nasaPhoto.explanation}</h3>
-      <img src={nasaPhoto.url} />
-    </div>
+    <Grid
+      textAlign="center"
+      style={{ height: "100vh", width: "90%", margin: "20px auto" }}
+      verticalAlign="middle"
+    >
+      <Header as="h1">
+        <Icon name="space shuttle" />
+        <Header.Content>{nasaPhoto.title}</Header.Content>
+      </Header>
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <Image src={nasaPhoto.url} />
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Container>{nasaPhoto.explanation}</Container>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+    // <div className="App">
+    //   {/* <p>
+    //     Read through the instructions in the README.md file to build your NASA
+    //     app! Have fun !
+    //   </p> */}
+    //   <h3>{nasaPhoto.explanation}</h3>
+    //   <img src={nasaPhoto.url} />
+    // </div>
   );
 }
 
